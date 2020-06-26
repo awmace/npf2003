658c1a4920f770605295372be3762b8a3c4ae763
npf <2638274890@qq.com>
2020-06-26 22:09:41 +0800

    在app1中的view文件添加了项目的第一个视图

--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    print('这是第一个试图')
+    return HttpResponse('Success')
